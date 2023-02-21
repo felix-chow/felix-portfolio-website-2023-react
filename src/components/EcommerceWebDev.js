3a01ebdaf95bb1965546afcca1192e2a9ed50cf2
felix-chow <dong_chow@yahoo.com>
2023-02-21 11:51:30 -0800

    Revised Cafe Paradise project description

--- src/components/EcommerceWebDev.js
+++ src/components/EcommerceWebDev.js
@@ -5,12 +5,14 @@ const EcommerceWebDev = () => {
             <div className="w-50 p-3" id="ecommerce" style={{ marginTop: "100px" }}>
                 <h2 style={{ fontSize: 40 }}>E-commerce Web Development</h2>
                 <h3>Cafe paradise</h3>
-                <p>Here's a coffee shop website I have created where customers can order coffee and donuts. The website stores user-selected products into a shopping cart and offers the option of either local delivery or store pick-up. Once the user proceeds to checkout, their contact and billing information will be stored during the checkout process before they confirm their purchase. The web pages have been created using Liquid, JSON, and CSS. ReactJS scripting is used for payment methods. Google Maps API calls are used for shipping and delivery. All information is stored in Shopify's database.</p>
+                <p>Here's a coffee shop website I have created where customers can order coffee and donuts. The website stores user-selected products into a shopping cart. After completing the checkout process, their order will be stored in the order history under their profile. The web pages have been created using ReactJS and CSS. All menu items and sample users have been seeded into the database via PostgreSQL. New users are inserted into the database upon registration. This website uses the Node Express server.</p>
                 <section className="skills">
                     <ul>
-                        <li>&#9989;Liquid</li>
                         <li>&#9989;JSON</li>
+                        <li>&#9989;PostgreSQL</li>
                         <li>&#9989;CSS</li>
+                        <li>&#9989;Express</li>
+                        <li>&#9989;NodeJS</li>
                         <li>&#9989;ReactJS</li>
                         <li>&#9989;API</li>
                     </ul>
